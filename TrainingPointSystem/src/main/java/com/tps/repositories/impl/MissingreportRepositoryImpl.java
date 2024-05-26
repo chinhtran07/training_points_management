ac78648dfdff1f60bff8e57510f47d6ee154374b
NguyenVanCanh <ngcanh1236@gmail.com>
2024-05-26 21:39:36 +0700

    merge

--- TrainingPointSystem/src/main/java/com/tps/repositories/impl/MissingreportRepositoryImpl.java
+++ TrainingPointSystem/src/main/java/com/tps/repositories/impl/MissingreportRepositoryImpl.java
@@ -1,7 +1,6 @@
 package com.tps.repositories.impl;
 
-import com.tps.pojo.Missingreport;
-import com.tps.pojo.Registermission;
+import com.tps.pojo.*;
 import com.tps.repositories.MissingReportRepository;
 import org.hibernate.Session;
 import org.hibernate.query.Query;
@@ -10,31 +9,53 @@ import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Join;
+import javax.persistence.criteria.Root;
+import java.util.List;
+
 @Repository
 @Transactional
-public class MissingreportRepositoryImpl implements MissingReportRepository {
+public class MissingReportRepositoryImpl implements MissingReportRepository {
+
     @Autowired
-    LocalSessionFactoryBean factoryBean;
+    private LocalSessionFactoryBean sessionFactory;
 
     @Override
-    public Missingreport getMissingByStudentMission(int studentId, int missionId) {
-        Session session = factoryBean.getObject().getCurrentSession();
-        Query query = session.createQuery("FROM Missingreport WHERE student.id=:studentId AND mission.id=:missionId");
-        query.setParameter("studentId", studentId);
-        query.setParameter("missionId", missionId);
-        return (Missingreport) query.uniqueResult();
-    }
+    public List<Object[]> getMissionReportByFaculty(int facultyId) {
+        Session session = this.sessionFactory.getObject().getCurrentSession();
+        CriteriaBuilder builder = session.getCriteriaBuilder();
+        CriteriaQuery<Object[]> criteria = builder.createQuery(Object[].class);
 
-    @Override
-    public void updateMissingreport(Missingreport missingreport) {
-        Session session = factoryBean.getObject().getCurrentSession();
-        session.update(missingreport);
-    }
+        Root<Student> root = criteria.from(Student.class);
+        Join<Student, User> userJoin = root.join("user");
+        Join<Student, Faculty> facultyJoin = root.join("faculty");
+        Join<Student, MissingReport> missingReportJoin = root.join("missingReports");
+        Join<MissingReport, Mission> missionJoin = missingReportJoin.join("mission");
 
-    @Override
-    public Missingreport addMissingreport(Missingreport missingreport) {
-        Session session = factoryBean.getObject().getCurrentSession();
-        session.save(missingreport);
-        return missingreport;
+        criteria.multiselect(
+                userJoin.get("lastName"),
+                userJoin.get("lastName"),
+                root.get("studentId"),
+                missionJoin.get("id"),
+                missingReportJoin.get("updatedDate"),
+                missingReportJoin.get("isActive")
+        );
+
+        criteria.where(builder.equal(facultyJoin.get("id"), facultyId));
+
+        criteria.orderBy(builder.asc(missionJoin.get("updatedDate")));
+
+        criteria.groupBy(userJoin.get("lastName"),
+                userJoin.get("lastName"),
+                root.get("studentId"),
+                missionJoin.get("id"),
+                missingReportJoin.get("updatedDate"),
+                missingReportJoin.get("isActive"));
+
+        Query query = session.createQuery(criteria);
+
+        return query.getResultList();
     }
 }
