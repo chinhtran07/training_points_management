a35d1b65186f504d4d8613c5efe640ff4e318764
chinhtran07 <chinh.t1807@gmail.com>
2024-06-15 18:00:43 +0700

    update api missing report student

--- TrainingPointSystem/src/main/java/com/tps/components/MissingReportConverter.java
+++ TrainingPointSystem/src/main/java/com/tps/components/MissingReportConverter.java
@@ -2,11 +2,10 @@ package com.tps.components;
 
 import com.tps.dto.MissingReportDTO;
 import com.tps.dto.MissingReportFacultyDTO;
+import com.tps.dto.MissingReportStudentDTO;
 import com.tps.pojo.MissingReport;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
-import java.text.SimpleDateFormat;
 
 @Component
 public class MissingReportConverter {
@@ -32,5 +31,18 @@ public class MissingReportConverter {
         missingReportDTO.setDescription(missingreport.getDescription());
 
         return missingReportDTO;
+    }
+
+    public MissingReportStudentDTO toMissingReportStudentDTO(Object[] o) {
+        MissingReportStudentDTO missingReportStudentDTO = new MissingReportStudentDTO();
+        missingReportStudentDTO.setPointGroupName(o[0].toString());
+        missingReportStudentDTO.setActivityName(o[1].toString());
+        missingReportStudentDTO.setMissionId(o[2].toString());
+        missingReportStudentDTO.setMissionName(o[3].toString());
+        missingReportStudentDTO.setMissionPoint((Integer) o[4]);
+        missingReportStudentDTO.setDescription(o[5].toString());
+        missingReportStudentDTO.setStatus(o[6].toString());
+
+        return missingReportStudentDTO;
     }
 }
